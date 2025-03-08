46d96dd0b98268b0d91d504daf72fbdf859b8964
sumin <mins19960091@gmail.com>
2025-03-08 18:25:09 +0900

    상품등록폼 타임리프 완성

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,11 +27,30 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    //Integer int
+    //Long long
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") Long itemId , Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
     //테스트용 데이터 추가(디비 인서트 안하고 추가 하는 방법)
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA",10000,10));
         itemRepository.save(new Item("itemB",20000,20));
+    }
+
+    @GetMapping("/addForm")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/addForm")
+    public String save(){
+        return "basic/addForm";
     }
 
 
