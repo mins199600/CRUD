52102ba74f29008cedd9288f117df477e62e904a
sumin <mins19960091@gmail.com>
2025-03-13 17:51:43 +0900

    상품수정로직

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -33,55 +33,26 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    //테스트용 데이터 추가(디비 인서트 안하고 추가 하는 방법)
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable("itemId") Long itemId , Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String edit(@PathVariable("itemId") Long itemId , Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+
+        //테스트용 데이터 추가(디비 인서트 안하고 추가 하는 방법)
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA",10000,10));
         itemRepository.save(new Item("itemB",20000,20));
     }
 
-    @GetMapping("/addForm")
-    public String addForm(){
-        return "basic/addForm";
-    }
-
-//    @PostMapping("/addForm")
-    public String addItemV1(@RequestParam("itemName") String itemName,
-                       @RequestParam("quantity") Integer quantity ,
-                       @RequestParam("price") int price,
-                       Model model) {
-        Item item = new Item();
-        item.setItemName(itemName);
-        item.setPrice(price);
-        item.setQuantity(quantity);
-
-        itemRepository.save(item);
-        model.addAttribute("item",item);
-
-        return "basic/item";
-    }
-
-//    @PostMapping("/addForm")
-    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
-
-        itemRepository.save(item);
-//        model.addAttribute("item",item); 자동추가, 생략가능
-
-        return "basic/item";
-    }
-
-//    @PostMapping("/addForm")
-    public String addItemV3(@ModelAttribute("item") Item item) {
-        itemRepository.save(item);
-        return "basic/item";
-    }
-    @PostMapping("/addForm")
-    public String addItemV4(Item item) {
-
-        itemRepository.save(item);
-
-        return "basic/item";
-    }
-
-
 }
+
